db7f3840c372d545492b37f53b94538219ec1184
nikkiptaylor <nicolept@stanford.edu>
2017-11-14 12:17:37 -0800

    added code to add variables for verbs

--- trainOnRecipes.py
+++ trainOnRecipes.py
@@ -3,12 +3,30 @@
 
 import recipeUtil.py
 
+
+
 def createCSP():
+  max_ingredients = 10
   csp = recipeUtil.CSP()
+  # add variable for each verb
+  with open('cooking_verbs.txt') as f:
+		verbs = f.readLines()
+	verbs = [x.strip() for x in verbs]
+  
+  for verb in verbs:
+    csp.add_variable(verb, [i for i in range(0, max_ingredients + 1)])
+  
   # add variable for each ingredient in cumulative ingredients list
+  
   # iterate over each sentence in directions 
   # - identify ingredients in sentence
   # - identify verbs in sentence
   # - add binary factor for each ingredient in each sentence and verbs in sentence
+  # - add binary factor for order of verbs in sentence
+  # - add binary factor for order of verbs from previous sentence to this sentence
+  # (ie weight assignment of verb from sentence 1 higher if the assignment of verb 1 < assignment of verb 2)
+  # add factors to constrain only one verb to each assignment 1-10 and only one ingredient to each assignment 1-10
+  
+  
   
   
